d867a687f0a766634e8b78b37f468584e4fba051
Armanur Rahman <mubeenarman19@gmail.com>
2023-08-05 02:05:22 +0900

    implement modal

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,35 @@
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm-v1";
+import Modal from "../../ui/Modal";
+
+function AddCabin() {
+    return (
+        <Modal>
+            <Modal.Open opens='cabin-form'>
+                <Button>Add new cabin</Button>
+            </Modal.Open>
+            <Modal.Window name='cabin-form'>
+                <CreateCabinForm />
+            </Modal.Window>
+
+            {/* <Modal.Open opens='table'>
+                <Button>Show table</Button>
+            </Modal.Open>
+            <Modal.Window name='table'>
+                <CreateCabinForm />
+            </Modal.Window> */}
+        </Modal>
+        // <div>
+        //     <Button onClick={() => setIsOpenModal((form) => !form)}>
+        //         Add new cabin
+        //     </Button>
+        //     {isOpenModal && (
+        //         <Modal>
+        //             <CreateCabinForm />
+        //         </Modal>
+        //     )}
+        // </div>
+    );
+}
+
+export default AddCabin;
--- src/features/cabins/CreateCabinForm-v1.jsx
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -8,7 +8,7 @@ import FormRow from "../../ui/FormRow";
 import useCreateCabin from "./useCreateCabin";
 import useEditCabin from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
     const { id: editId, ...editValues } = cabinToEdit;
     const isEditSession = Boolean(editId);
 
@@ -32,7 +32,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
             editCabin(
                 { newCabinData: { ...data, image }, id: editId },
                 {
-                    onSuccess: reset(),
+                    onSuccess: () => {
+                        reset();
+                        onCloseModal?.();
+                    },
                 }
             );
         } else {
@@ -48,7 +51,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         console.log(error);
     }
     return (
-        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Form
+            onSubmit={handleSubmit(onSubmit, onError)}
+            type={onCloseModal ? "modal" : "regular"}
+        >
             <FormRow label='Cabin name' error={errors?.name?.message}>
                 <Input
                     type='text'
@@ -134,7 +140,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation='secondary' type='reset'>
+                <Button
+                    variation='secondary'
+                    type='reset'
+                    onClick={onCloseModal}
+                >
                     Cancel
                 </Button>
                 <Button disabled={isWorking}>
