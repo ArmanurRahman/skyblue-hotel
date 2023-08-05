d68efa8acf1d1ad50823c1b56fea3c1404a097c5
Armanur Rahman <mubeenarman19@gmail.com>
2023-08-05 15:11:45 +0900

    implement menu option

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import CabinRow from "./CabinRow";
 import Spinner from "../../ui/Spinner";
 import useCabin from "./useCabin";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
     display: grid;
@@ -28,20 +29,24 @@ function CabinTable() {
         return <Spinner />;
     }
     return (
-        <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-            <Table.Header role='row'>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
-            <Table.Body
-                data={cabins}
-                render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
-            />
-        </Table>
+        <Menus>
+            <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+                <Table.Header role='row'>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
+                <Table.Body
+                    data={cabins}
+                    render={(cabin) => (
+                        <CabinRow key={cabin.id} cabin={cabin} />
+                    )}
+                />
+            </Table>
+        </Menus>
     );
 }
 
