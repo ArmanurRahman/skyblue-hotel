1b1d01d2580b808542cb66d80917be372c19e59a
Armanur Rahman <mubeenarman19@gmail.com>
2023-08-05 16:23:27 +0900

    implement filter function in cabin table

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -65,7 +65,7 @@ const TableContext = createContext();
 const Table = ({ columns, children }) => {
     return (
         <TableContext.Provider value={{ columns }}>
-            <StyledTable role='table'>{children} </StyledTable>;
+            <StyledTable role='table'>{children} </StyledTable>
         </TableContext.Provider>
     );
 };
@@ -92,7 +92,7 @@ const Body = ({ data, render }) => {
     if (!data.length) {
         return <Empty>No data to show</Empty>;
     }
-    return <StyledBody>{data.map(render)}</StyledBody>;
+    return <StyledBody>{data?.map(render)}</StyledBody>;
 };
 
 Table.Header = Header;
