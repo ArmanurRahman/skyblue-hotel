9bccea8872391f3958a674f81430bd163f2b68c5
Armanur Rahman <mubeenarman19@gmail.com>
2023-08-05 20:35:29 +0900

    implement filter in booking table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 const useBooking = () => {
+    const [searchParam, setSearchParam] = useSearchParams();
+    const filterValue = searchParam.get("status");
+
+    const filter =
+        !filterValue || filterValue === "all"
+            ? null
+            : { field: "status", value: filterValue };
     const {
         isLoading,
         data: bookings,
         error,
     } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings,
+        queryKey: ["bookings", filter],
+        queryFn: () => getBookings({ filter }),
     });
     return { isLoading, bookings, error };
 };
